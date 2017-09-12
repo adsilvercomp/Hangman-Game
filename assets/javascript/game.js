58d6be6e45d64b242e846852c83a089757d1816a
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-09-12 14:31:20 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,24 @@
  	var currentWord = ["zebra", "dog"];
     var wrongLetters= [];
     var numGuesses = 9;
-    var winCounter;
-    var lossCounter;
+    var winCounter=0;
+    var lossCounter=0;
     //the variable animal randomly selects a word from the array currentWord.
-    var animal = currentWord[Math.floor(Math.random() * currentWord.length)];
+    var animal=currentWord[Math.floor(Math.random() * currentWord.length)];
     //seperateLetters makes each letter in animal it's own array.
-    var seperateLetters= animal.split("");
+    var seperateLetters=animal.split("");
     
     
     
    //this for loop substitutes dashes for letters in randomly selected animal.
+        
         for(var i=0; i<animal.length ; i++){
             animal =animal.replace(animal[i], "-");
         }
+
     
+
+        
     
     
 
@@ -25,31 +29,39 @@
     //the User's gussed letter is stored in the variable userInput.
     document.onkeyup = function guess() {
 
-        var userInput= String.fromCharCode(event.keyCode).toLowerCase();      
+            var userInput= String.fromCharCode(event.keyCode).toLowerCase();      
 
-         console.log(userInput) 
+            console.log(userInput) 
 
             //this for loop loops through each letter in the randomly chosen animal.
             for(var x=0; x<seperateLetters.length ;x++){
-                //if the userInput === any of the letters in the randomly chosen animal an alert will pop up
-                //the userInput will replace the dash of the letter it matched with.
-                 if(userInput===seperateLetters[x]){
-                   alert("match");
-                   animal= animal.replace("-", userInput);
-                   interface();
-                }  
-
-
-            }     
-                //if the userInput does not match the letters in the randomly chosen animal 
-                //push the userInput to the wrongLetters array.
-                //subtract the number of guesses the user has left.
-                //indexOf is not working because?
-                if(userInput===seperateLetters.indexOf(-1)){                
-                        wrongLetters.push(userInput);
-                        numGuesses--;
+                    //if the userInput === any of the letters in the randomly chosen animal an alert will pop up
+                    //the userInput will replace the dash of the letter it matched with.
+                     if(userInput===seperateLetters[x]){
+                       alert("match");
+                       animal= animal.replace("-", userInput);
                        interface();
-                    }   
+                    } 
+
+                    //if all of the dashes are replaced by letters, the animal word resets and wins++
+                     if(animal===animal.replace("-", userInput)){
+                        winCounter++;
+                        generate();
+                        interface();
+                        
+                     }
+
+
+                }     
+                    //if the userInput does not match the letters in the randomly chosen animal 
+                    //push the userInput to the wrongLetters array.
+                    //subtract the number of guesses the user has left.
+                    //indexOf is not working because?
+                    if(userInput===seperateLetters.indexOf(-1)){                
+                            wrongLetters.push(userInput);
+                            numGuesses--;
+                           interface();
+                        }   
 
 
              
@@ -58,10 +70,14 @@
     }
 
  
-    
-    
-       
-    	
+    function generate(){
+            var animal=currentWord[Math.floor(Math.random() * currentWord.length)];
+            var seperateLetters=animal.split("");
+
+            for(var i=0; i<animal.length ; i++){
+            animal =animal.replace(animal[i], "-");
+        }
+    }
     
 
 
@@ -87,6 +103,7 @@
     //this function calls the the interface when the window loads.
     window.onload = function(){
         interface();
+        generate();
     }
 	
 
