b5b47ca480545617222f7689615e9adb78457838
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-09-11 22:35:52 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,65 @@
-
-					// User charAt() method to isloate individual letters in an array
-					// indexOf() method shows the first time a letter appears in a word
-
-					//question:Is there a value you could place in charAt that spans entire word?
-
-
-
-
-// 1.Create a bank of words with a common theme. (an array)
-
-// 2.User clicks any key to begin.
-
-// 3.computer randomly generates a word 
-
-// 4. computer obscures letters of word with dashes.
-
-// 4. user guesses a letter
-
-// if the letter is correct it replaces a dash and no guesses are deducted
-
-// if the letter is wrong, the computer displays the wrong letter and the number of guesses a user has left.
-
-// if the player loses an image of a hanged man appears and Chopin's death march plays. 
-
-// if the player wins a new word is generated.  
+ 	
+	//global variables
+ 	var currentWord = ["zebra", "dog"];
+    var wrongLetters = [];
+    var numGuesses = 9;
+    var winCounter;
+    var lossCounter;
+    var userInput;
+    //the variable animal randomly selects a word from the array currentWord.
+    var animal = currentWord[Math.floor(Math.random() * currentWord.length)];
+    var seperateLetters= animal.split("");
+    
+    
+    
+   
+    
+    
+    
 
 
+    //the function guess is called by the user's key click.
+    //the User's gussed letter is stored in the variable userInput.
+    document.onkeyup = function guess() {
 
+     userInput= String.fromCharCode(event.keyCode).toLowerCase();      
 
+     console.log(userInput)  
+   
+    }
 
-
-
-
-
-// 1.Create a bank of words with a common theme. (an array)
-var composers = ["mozart", "beethoven" ,"stravinsky"];
-var name = encrypt();
-var guesses=10;
-var nameGenerate= composers[Math.floor(Math.random()*composers.length)];
-
-
-//  computer obscures letters of word with dashes.
-	
-
-	function encrypt(){
-	for(var i=0; i<nameGenerate.length; i++){
-		return nameGenerate.charAt(i)="-";
-
-
-
-
-	};
  
+    //this function substitutes dashes for letters in randomly selected animal.
+        for(var i=0; i<animal.length ; i++){
+            animal =animal.replace(animal[i], "-");
+        }
+    
+        //for loop ensure that the dashes take up length of animal name
+    	
+    
 
 
 
-// 1.User clicks any key to begin. -things in this function will happen once per round.
-document.onkeyup=function(){
+        
+    //this function pushes user interface to the html file.
+    function interface() {
+        var html =  "<h1>  Hangman  </h1>" + 
+                    "<h2> Guess the Animal </h2>" +
+                     "<h1>" + animal + "</h1>" +
+                    "<h2> Wrong Guesses: " + wrongLetters + "</h2>" +
+                    "<h2> Guesses Left: " + numGuesses + "</h2>" +
+                    "<h2> Wins: " + winCounter + "</h2>" +
+                    "<h2> Losses: " + lossCounter + "</h2>";
 
-	// 2.computer randomly generates a word 
+        document.getElementById("game").innerHTML=html;
+                    
+    }
 
-	encrypt();
 
-}
-
-	
-	// things in this function will happen multiple times throughout a round of gameplay.
-	function gamePlay(nameGenerate, userGuess){
-
-	// 4. user guesses a letter	
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-		console.log(userGuess); 
-
-	//5. if the letter is correct it replaces a dash and no guesses are deducted
-
-	for(var x=0; x<nameGenerate.length; x++){
-		if( nameGenerate.charAt(x)==="userGuess"){
-				nameGenerate = nameGenerate.replace(/userGuess/,nameGenerate(x));
-		}else{
-			guesses--;
-			alert("Guesses: " - guesses);
-		}
-
-}
-
-}
+    //this function calls the the interface when the window loads.
+    window.onload = function(){
+        interface();
+    }
 	
 
 
