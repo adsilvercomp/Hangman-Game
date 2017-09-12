45d6489cd10cee4163c8bd2effb6ab41a8aa72d0
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-09-12 12:55:11 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,12 @@
     var lossCounter;
     //the variable animal randomly selects a word from the array currentWord.
     var animal = currentWord[Math.floor(Math.random() * currentWord.length)];
+    //seperateLetters makes each letter in animal it's own array.
     var seperateLetters= animal.split("");
     
     
     
-   //this function substitutes dashes for letters in randomly selected animal.
+   //this for loop substitutes dashes for letters in randomly selected animal.
         for(var i=0; i<animal.length ; i++){
             animal =animal.replace(animal[i], "-");
         }
@@ -28,20 +29,30 @@
 
          console.log(userInput) 
 
-            for(var x=0; x<seperateLetters.length;x++){
+            //this for loop loops through each letter in the randomly chosen animal.
+            for(var x=0; x<seperateLetters.length ;x++){
+                //if the userInput === any of the letters in the randomly chosen animal an alert will pop up
+                //the userInput will replace the dash of the letter it matched with.
                  if(userInput===seperateLetters[x]){
                    alert("match");
-
+                   animal= animal.replace("-", userInput);
+                   interface();
                 }  
 
+
             }     
+                //if the userInput does not match the letters in the randomly chosen animal 
+                //push the userInput to the wrongLetters array.
+                //subtract the number of guesses the user has left.
+                //indexOf is not working because?
+                if(userInput===seperateLetters.indexOf(-1)){                
+                        wrongLetters.push(userInput);
+                        numGuesses--;
+                       interface();
+                    }   
 
 
-             if(userInput!==seperateLetters.indexOf(userInput)){                
-                    wrongLetters.push(userInput);
-                   interface();
-
-                }   
+             
 
            
     }
